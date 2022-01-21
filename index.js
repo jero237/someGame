641b8e790f0959a5dd88179191a1bd3643976b27
jero237 <jeronimobalestra@gmail.com>
2022-01-21 15:31:58 -0300

    Ultima version

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
-$(document).ready(function(){
-    const MAIN = $("#main")
-    
-    for(let i = 0; i < 20; i++){
-        for(let j = 0; j < 20; j++){
-            $("<div id='" + i + "-" + j + "' class='square' style='grid-area: " + i + "/" + j + "/" + (i+1) + "/" + (j+1) + "' />" ).appendTo(MAIN);
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+
+$(document).ready(function () {
+    const MAIN = $("#playArea")
+    const SQUARE_DEFAULT_COLOR = "red";
+    const SCORE = $("#score")
+    const LIVES = [$("#live1"), $("#live2"), $("#live3")]
+    const PLAY_BUTTON = $(".fa-play-circle")
+
+
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 20; j++) {
+            $("<div id='" + i + "-" + j + "' class='square' style='grid-area: " + i + "/" + j + "/" + (i + 1) + "/" + (j + 1) + "' />").appendTo(MAIN);
         }
     }
 
 
-    $(".square").hover(function(){
-        $(this).css("background-color","yellow")
+    $(".square").hover(function () {
+        $(this).css("background-color", "yellow")
+    }, function () {
+        $(this).css("background-color", SQUARE_DEFAULT_COLOR)
     })
 
 
+    async function play() {
+        let randomPositionX;
+        let randomPositionY;
+        let puntaje = 0;
+        let waitTime = 1000;
+        let hovered = false;
+        let lives = 3;
+
+        for (let i = 0; i < 3; i++) {
+            LIVES[i].removeClass("far")
+            LIVES[i].addClass("fas")
+        }
+
+
+        const sumarPuntaje = () => {
+            puntaje++;
+            SCORE.text(puntaje)
+            console.log(waitTime)
+            hovered = true;
+            $("#" + randomPositionX + "-" + randomPositionY).off("mouseenter", sumarPuntaje)
+        }
+
+        const lostLive = () => {
+            LIVES[lives - 1].removeClass("fas")
+            LIVES[lives - 1].addClass("far")
+            lives--;
+        }
+
+        for (let i = 0; i < 1000; i++) {
+
+            randomPositionX = Math.floor(Math.random() * (19 - 0 + 1) + 0)
+            randomPositionY = Math.floor(Math.random() * (19 - 0 + 1) + 0)
+
+            $("#" + randomPositionX + "-" + randomPositionY).css("background-color", "blue")
+
+            $("#" + randomPositionX + "-" + randomPositionY).on("mouseenter", sumarPuntaje)
+
+
+            console.log(waitTime)
+
+            await timer(waitTime)
+            if (!hovered) lostLive()
+
+            $("#" + randomPositionX + "-" + randomPositionY).off("mouseenter", sumarPuntaje)
+            $("#" + randomPositionX + "-" + randomPositionY).css("background-color", SQUARE_DEFAULT_COLOR)
+            hovered = false;
+
+            if (lives == 0) {
+                SCORE.text("0")
+                return
+            }
+        }
+    }
+
+
+
+    PLAY_BUTTON.click(play)
 
 })
